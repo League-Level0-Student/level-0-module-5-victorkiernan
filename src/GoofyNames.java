dde793703612143cd3a5f57714b2142e55e5dda2
kiernan.victor <kiernan.victor@SEMS-259-07.SEMS259.LAB>
2018-04-25 14:56:47 -0700

    goofynames- ip

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -11,9 +11,10 @@ public class GoofyNames {
 
 		// 1. Ask the user to enter their name
 
+String name=		JOptionPane.showInputDialog("What be ye name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
 
